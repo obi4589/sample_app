3b840cd8a5652209ab4a080d219f6a25c6bb45cc
Obinna Ezebuiroh <ove201@nyu.edu>
2014-09-08 21:44:23 -0400

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,7 +16,7 @@ describe "Static pages" do
 
 
 
-
+#this is an original test section for reference
   describe "Help page" do
 
     it "should have the content 'Help'" do
@@ -29,32 +29,44 @@ describe "Static pages" do
       expect(page).to have_title("#{base_title} | Help")
     end
   end
+#this is the original test section for reference
+
 
   describe "About page" do
+    before { visit about_path}
 
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("#{base_title} | About Us")
-    end
+    it {should have_content('About Us')}
+    it {should have_title(full_title('About Us'))}
   end
+
+
 
   describe "Contact page" do
+    before { visit contact_path}
 
-    it "should have the content 'Contact Us'" do
-      visit contact_path
-      expect(page).to have_content('Contact Us')
-    end
-
-    it "should have the title 'Contact Us'" do
-      visit contact_path
-      expect(page).to have_title("#{base_title} | Contact Us")
-    end
+    it {should have_selector('h1', text: 'Contact Us')}
+    it {should have_title(full_title('Contact Us'))}
   end
+
+
+
+it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact Us'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
+
+
+
 end
 
 
